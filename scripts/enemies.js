e488a935e3357677237abcc3bc9230a75f50d644
majkelmichel <51023578+majkelmichel@users.noreply.github.com>
2020-05-26 12:00:45 +0200

    ufo się porusza

--- scripts/enemies.js
+++ scripts/enemies.js
@@ -1,39 +1,35 @@
 var leftOffset = document.getElementById('healthBar').offsetWidth;
 
-var ufo = function (x, id) {
+var ufo = function (x) {
     this.x = x;
     this.y = 10;
     this.height = 100;
     this.width = 100;
-    this.id = id;
     this.x = Math.min(this.x, document.getElementById('playField').offsetWidth - this.width);
+    
+    var imgHtml = '<img src="img/ufo.png">';
+    var imgElement = $(imgHtml);
+    
+    this.drawImg = function () {
+        imgElement.css({
+            position: "absolute",
+            left: this.x + leftOffset,
+            top: this.y
+        });
+        $("body").append(imgElement);
+    };
+    
+    this.updateCSS = function () {
+        var prevY = imgElement[0].style.top;
+        prevY = parseInt(prevY);
+        prevY += 3;
+        var nextY = prevY + "px";
+        imgElement[0].style.top = nextY;
+    };
 }
 var x = Math.floor(Math.random() * document.getElementById('playField').offsetWidth);
 
-ufo.prototype.drawImg = function () {
-    var imgHtml = '<img src="img/ufo.png">';
-    this.imgElement = $(imgHtml);
-    
-    this.imgElement.css({
-        position: "absolute",
-        left: this.x + leftOffset,
-        top: this.y
-    });
-    $("body").append(this.imgElement);
-};
 
-ufo.prototype.updateCSS = function (y) {
-    var prevY = this.imgElement[0].style.top;
-    prevY = parseInt(prevY);
-    prevY += 3;
-    var nextY = prevY + "px";
-    this.imgElement[0].style.top = nextY;
-}
-
-ufo.prototype.moveDown = function () {
-    var y = this.y - 10;
-    return y;
-}
 //
 //ufo.prototype.randomMove = function () {
 //    var randX = Math.floor(Math.random() * document.getElementById('playField').offsetWidth);
